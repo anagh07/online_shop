009ca323822e9c047a374841b7b0a960ff780897
Anagh Mehran <anagh07@gmail.com>
2020-07-17 00:38:42 -0400

    finished ejs

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 const adminData = require('./routes/admin');
 const shoproute = require('./routes/shop');
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyparser.urlencoded({ extended: false }));
@@ -23,7 +23,7 @@ app.use(shoproute);
 app.use((req, res) => {
   console.log('Error 404');
   // res.status(404).sendFile(path.join(rootdir, 'views', 'error.html'));
-  res.status(404).render('404', { pageTitle: 'error' });
+  res.status(404).render('404', { pageTitle: 'error', path: '404' });
 });
 
 const PORT = 3000;
